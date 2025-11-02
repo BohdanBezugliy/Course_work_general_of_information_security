7df11cc94786bcf789ebabd8aeb6022cd40ff631
Bohdan <131963567+BohdanBezugliy@users.noreply.github.com>
2025-11-02 02:53:29 +0200

    Fixed classes User and UserManager(system of directory access was made more simple)

--- UserManager.py
+++ UserManager.py
@@ -1,29 +1,24 @@
 import re
+from os import access
+
 
 class UserManager:
 
     def __init__(self):
-        users = []
         self.__names=[]
         with open("nameuser.txt", "r") as f:
-            users = f.read().split("\n")
+            for line in f.readlines():
+                self.__names.append(line.split(" ")[0])
         self.__N = 15
-        self.__n = self.__N - len(users)
-        for user in users:
-            self.__names.append(user.split(" ")[0])
+        self.__n = self.__N - len(self.__names)
 
+    def get_users(self):
+        return self.__names
 
-    def GetUsers(self):
-        return self.__users
-
-    def GetN(self):
-        return self.__N
-
-
-    def addUser(self):
+    def create_user(self):
         if self.__n <= 0:
             print("Неможливо додати нового користувача!")
-            return
+            return None
         with open("nameuser.txt", "a") as f:
             reg_pattern_name = re.compile(r"^.{3,50}$")
             while True:
@@ -34,7 +29,7 @@ class UserManager:
                             "Імʼя користувача вже зайняте, оберіть інше!\nЯкщо бажаєте відмінити операцію введіть cenc, "
                             "щоб продвжити натисніть Enter")
                         if input() == "cenc":
-                            return
+                            return None
                     else:
                         break
                 else:
@@ -42,45 +37,38 @@ class UserManager:
                           "Якщо бажаєте відмінити операцію введіть cenc, "
                             "щоб продвжити натисніть Enter")
                     if input() == "cenc":
-                        return
+                        return None
             reg_pattern_passw = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,32}$")
+
             while True:
                 passw = input("Введіть пароль для нового користувача:")
                 if re.search(reg_pattern_passw, passw):
-                    disks = ["A", "B", "C", "D", "E"]
-                    access = ""
-                    for disk in disks:
-                        while True:
-                            rule = input(f"Оберіть для каталогу {disk}\nнаступні дозволи(Введіть символи через кому)"
-                                f"\n(Е-виконання, R-читання, W-запис, "
-                                f"А-доповнення, мінімальні R або Е, середні R, Е, або R, Е, А):")
-                            reg_pattern_access =re.compile(
-                                r"^(?!.*E.*,E.*)"
-                                r"(?!.*R.*,R.*)"
-                                r"(?!.*W.*,W.*)" 
-                                r"(?!.*A.*,A.*)"
-                                r"(N|([ERWA](?:,[ERWA]){0,3}))$"
-                            )
-                            if re.search(reg_pattern_access, rule):
-                                if disk =="E":
-                                    access+=disk+"("+rule+")"
-                                else:
-                                    access+=disk+"("+rule+"):"
-                                break
-                            else:
-                                print("Введіть дозволи в правильному форматі!")
-                    f.write(f"\n{username} {passw} {access}")
-                    print(len(self.__names))
-                    from User import User
-                    return User(username, len(self.__names)-1)
+                    break
                 else:
                     print("Пароль має містити в собі\nхоча б одну велику та малу літери\nта цифру і довжиною має бути"
                         "не менше 8 та не більше 32 символів, оберіть інший!\n"
                         "Якщо бажаєте відмінити операцію введіть cenc, щоб продвжити натисніть Enter")
                     if input().lower()=="cenc":
-                        return
+                        return None
 
-    def DeleteUser(self,User):
+            disks = ["A", "B", "C", "D", "E"]
+            access = ""
+            for disk in disks:
+                while True:
+                    lvl_access = input(f"Оберіть для каталогу {disk} рівень доступу(1-повний,2-тільки перегляд та виконання): ")
+                    if lvl_access == "1" or lvl_access == "2":
+                        if disk == "E":
+                            access += f"{disk}" + f"{lvl_access}"
+                        else:
+                            access += f"{disk}" + f"{lvl_access}" + ","
+                        break
+                    else:
+                        print("Введіть рівень доступу в правильному форматі!")
+            f.write(f"\n{username} {passw} {access}")
+            from User import User
+            return User(username)
+
+    def remove_user(self,User):
         with open("nameuser.txt", "r") as f:
             file_content = f.readlines()
         new_file =[]
