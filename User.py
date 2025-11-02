43022c4a2b77ad99fab2ac08fb43960327d2a3e3
Bohdan <131963567+BohdanBezugliy@users.noreply.github.com>
2025-11-02 21:55:21 +0200

    Added checking date of creation password

--- User.py
+++ User.py
@@ -1,14 +1,25 @@
+import datetime
+from datetime import datetime,timedelta
+
+
 class User:
 
     def __init__(self, name):
         self.__name = name
+        safe_period_passw = timedelta(days=90)
+        with open("nameuser.txt", "r") as file:
+            for line in file.readlines():
+                if line.split(" ")[0] == self.__name:
+                    if datetime.strptime(line.split(" ")[3].replace("\n",""), '%d.%m.%y,%H:%M:%S')+safe_period_passw < datetime.now():
+                        print("Час змінити пароль!")
+                    else:
+                        print("Пароль досі актуальний!")
 
     def get_user_lvl(self):
         with open("nameuser.txt", "r") as file:
             for line in file.readlines():
                 if line.split(" ")[0] == self.__name:
                     return line.split(" ")[2]
-            return None
 
     def get_name(self):
         return  self.__name
